40cad913b5fad8e92e780e7e503b1b527080ab52
Andy C. Wong <wongandy31@gmail.com>
2018-09-03 18:40:54 +0800

    all 16 rooms visible

--- application/views/dashboard_table_result3.php
+++ application/views/dashboard_table_result3.php
@@ -1,9 +1,9 @@
-<table class="room-tracker table">
+<table class="room-tracker table" style="width: 65%">
 <thead>
 <tr>
 <?php
 foreach($rooms as $room_key => $room_val){
-	echo '<th style="color: black;font-weight: 600;font-size: 20px;">'.$room_val->name.'</th>';
+	echo '<th style="color: black;font-weight: 600;font-size: 12px;">'.$room_val->name.'</th>';
 }
 ?>
 </tr>
@@ -21,7 +21,7 @@ if($this->session->userdata('role')!= 3):
 			$check_in = $exploded[1];
 			
 			if($room_val->id==$room_id AND $draw_td==0){
-				echo '<td>';
+				echo '<td style="padding: 0;">';
 				$draw_td++;
 				$have_reservation++;
 			}
@@ -30,18 +30,18 @@ if($this->session->userdata('role')!= 3):
 				$theValues = $ro_val;
 				
 				if(strtotime($theValues[2])<=strtotime(date('Y-m-d H:i:s')) AND strtotime($theValues[3])>=strtotime(date('Y-m-d H:i:s')))
-					$active = '<label><u style="color: black;font-weight: 900;font-size: 16px;">'.date('h:i a', strtotime($theValues[2])).' - '.date('h:i a', strtotime($theValues[3])).'</u></label>';
+					$active = '<label><u style="color: black;font-weight: bold;" font-size:12px;">'.date('h:i a', strtotime($theValues[2])).' - '.date('h:i a', strtotime($theValues[3])).'</u></label>';
 				else
-					$active = '<label style="color: black;font-weight: 900;font-size: 16px;">'.date('h:i a', strtotime($theValues[2])).' - '.date('h:i a', strtotime($theValues[3])).' </label>';
+					$active = '<label style="color: black;font-weight: bold;" font-size:12px;">'.date('h:i a', strtotime($theValues[2])).' - '.date('h:i a', strtotime($theValues[3])).' </label>';
 					
 				if(strtotime($theValues[2])<=strtotime(date('Y-m-d H:i:s')) AND strtotime($theValues[3])>=strtotime(date('Y-m-d H:i:s')))
-					$active2 = '<span class="panel-eyecandy-title"><u style="color: black;font-weight: 900;font-size: 16px;">'.$theValues[4].'</u></span>';
+					$active2 = '<span class="panel-eyecandy-title"><u style="color: black;font-weight: bold;" font-size:12px;">'.$theValues[4].'</u></span>';
 				else
-					$active2 = '<span class="panel-eyecandy-title" style="color: black;font-weight: 900;font-size: 16px;">'.$theValues[4].'</span>';
+					$active2 = '<span class="panel-eyecandy-title" style="color: black;font-weight: bold;" font-size:12px;">'.$theValues[4].'</span>';
 				
 				echo '
 					<a href="javascript:void(0)" id="additional-person" room_id="'.$room_val->id.'" check_in="'.date('h|i|a', strtotime($theValues[2])).'" mrt_id="'.$theValues[0].'" current_person="'.$theValues[1].'" room_name="'.$room_val->name.'" movie_name="'.$theValues[4].'">
-					<div class="panel panel-primary text-center no-boder" style="width:110px;height:220px;">
+					<div class="panel panel-primary text-center no-boder" style="width:70px;">
 						<div class="panel-body '.($theValues[1]==0?($vacant_near_end_id==$theValues[0]?'yellow':'white'):($room_near_end_id==$theValues[0]?'red':'blue')).'">
 							
 								<i class="fa fa-users fa-3x"></i>
@@ -58,12 +58,12 @@ if($this->session->userdata('role')!= 3):
 		}#LOOP ROOM OCCUPANCY
 		if($have_reservation==0){
 			echo '
-			<td>
+			<td style="padding: 0;">
 				<a href="javascript:void(0)" class="movie-room-check-in" room_id="'.$room_val->id.'" check_in_time="'.date('H:i',strtotime(date('Y-m-d H:i:s'))+60).'">
-					<div class="panel panel-primary text-center no-boder" style="width:110px;">
+					<div class="panel panel-primary text-center no-boder" style="width:70px;">
 						<div class="panel-body white">
 								<i class="fa fa-check-square fa-3x"></i>
-							<label style="color: black;font-weight: 900;font-size: 16px;">VACANT</label>
+							<label style="color: black;font-weight: bold; font-size:12px;">VACANT</label>
 						</div>
 						<div class="panel-footer">
 							<span class="panel-eyecandy-title">Movie Room Check In
